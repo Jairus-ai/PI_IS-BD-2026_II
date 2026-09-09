35760df82da0d2bac9312efb8c3f75c110885dd1
Jesús Porras <jesus.porrasarguedas@ucr.ac.cr>
2026-09-09 10:36:26 -0600

    Resize the footer elements to make the footer smaller

--- cinema_app/src/components/Footer.jsx
+++ cinema_app/src/components/Footer.jsx
@@ -10,25 +10,25 @@ function Footer()
 
             <div id = "sobreNosotros">
                 <div id = "sobreNosotrosTitle">
-                    <p className='subtitleTextBold'>Sobre Nosotros</p>
+                    <p className='smallDetailTextBold'>Sobre Nosotros</p>
                     <hr/>
                 </div>
                 <ul id="sobreNosotrosList">
-                    <li className='detailText'>Historia</li>
-                    <li className='detailText'>Misión</li>
-                    <li className='detailText'>Visión</li>
+                    <li className='smallDetailText'>Historia</li>
+                    <li className='smallDetailText'>Misión</li>
+                    <li className='smallDetailText'>Visión</li>
                 </ul>
             </div>
 
             <div id = "contacto">
                 <div id = "contactoTitle">
-                    <p className='subtitleTextBold'>Contacto</p>
+                    <p className='smallDetailTextBold'>Contacto</p>
                     <hr/>
                 </div>
                 <ul id = "contactoList">
-                    <li className='detailText'>Correo</li>
-                    <li className='detailText'>Facebook</li>
-                    <li className='detailText'>Número de teléfono</li>
+                    <li className='smallDetailText'>Correo</li>
+                    <li className='smallDetailText'>Facebook</li>
+                    <li className='smallDetailText'>Número de teléfono</li>
                 </ul>
             </div>
         </footer>
--- cinema_app/src/components/NavBar.jsx
+++ cinema_app/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar()
     return(
         <div id = "navigationBar">
             <img id = "logoImageNavBar" src={logo} alt="logo" />
-            <p id = "logInButton" className='subtitleTextBold'>Iniciar Sesión</p>
+            <p id = "logInButton" className='detailTextBold'>Iniciar Sesión</p>
         </div>
     )
 }
