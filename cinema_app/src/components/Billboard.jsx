46ddf46e896b74c74e83a1a61d70fd59206631c1
WebitoWabito <ariasvargasadrian@gmail.com>
2026-09-09 11:21:46 -0600

    Delete suggestion's synopsis for now

--- cinema_app/src/components/Billboard.jsx
+++ cinema_app/src/components/Billboard.jsx
@@ -16,19 +16,16 @@ function Billboard()
                 <div className='billboardHolder'>
                     <img className = 'moviePoster' id = "movie1" src={movie1} alt="movie1" />
                     <p className='subtitleTextBold'>Spider Man</p>
-                    <p className='smallDetailText'>Han pasado cuatro años desde los acontecimientos de No Way Home, y Peter Parker ahora es un adulto que vive completamente solo, ha desaparecido voluntariamente de las vidas y recuerdos de quienes ama. Combatiendo el crimen en una Nueva York que ya no conoce su nombre, se ha dedicado por completo a proteger su ciudad—un Spider-Man a tiempo completo—, pero a medida que aumentan las exigencias sobre él, la presión desencadena una evolución física que amenaza su existencia, al mismo tiempo que un extraño nuevo patrón de crímenes da lugar a una de las amenazas más poderosas a las que se ha enfrentado.</p>
                 </div>
 
                 <div className='billboardHolder'>
                     <img className = 'moviePoster' id = "movie2" src={movie2} alt="movie1" />
                     <p className='subtitleTextBold'>Paw Patrol</p>
-                    <p className='smallDetailText'>Después de que el barco quede atrapado en una misteriosa tormenta, los cachorros de Paw Patrol naufragan en una isla tropical inexplorada repleta de dinosaurios. Allí conocen a Rex, un cachorro que lleva años varado en la isla y que se ha convertido en un auténtico experto en todo lo relacionado con los dinos. Cuando el archienemigo de Paw Patrol, el alcalde Humdinger, comienza a explotar la isla de forma imprudente con la intención de aprovechar sus recursos naturales, provoca accidentalmente la erupción de un enorme volcán inactivo.</p>
                 </div>
 
                 <div className='billboardHolder'>
                     <img className = 'moviePoster' id = "movie3" src={movie3} alt="movie1" />
                     <p className='subtitleTextBold'>Transformers</p>
-                    <p className='smallDetailText'>Los Autobots deben detener a un colosal robot consumidor de planetas que persigue la Matriz de Liderazgo Autobot. Al mismo tiempo, deben defenderse de un ataque total de los Decepticons.</p>
                 </div>
                 <img className='arrow' src={rightArrow} alt="" />
             </div>
